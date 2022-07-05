4fefdfc1d37592d83a3df170e249faf734ae1f3e
Kupriienko <Kupriienko>
2022-07-05 13:44:04 +0300

    input validation

--- calculation.js
+++ calculation.js
@@ -1,17 +1,35 @@
 function get_input() {
-    return document.getElementById('num_expression');
+    return document.getElementById("num_expression");
 }
 
 function number(clicked_id) {
-    get_input().value += clicked_id;
+    if (get_input().value === "0" && clicked_id !== "0") {
+        get_input().value = clicked_id;
+    } else if (!((get_input().value === "0" || get_input().value === "") && clicked_id === "0")) {
+        get_input().value += clicked_id;
+    }
 }
 
 function point() {
-    if (!(get_input().value.includes(".") || get_input().value === '')) {
+    if (!(get_input().value.includes(".") || get_input().value === "")) {
         get_input().value += ".";
     }
 }
 
 function reset() {
     get_input().value = "0";
+}
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode === 48 && get_input().value === "0") {
+        return false;
+    } else if (get_input().value === "0" && !(charCode > 31 && (charCode < 48 || charCode > 57))) {
+        get_input().value = String.fromCharCode(charCode)
+        return false
+    } else if (charCode === 46 && !(get_input().value.includes(".") || get_input().value === "")) {
+        return true;
+    } else {
+        return !(charCode > 31 && (charCode < 48 || charCode > 57));
+    }
 }
