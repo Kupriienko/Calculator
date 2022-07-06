8e1f6e495fa9fab0d1cc0b35431db41b1593e176
Kupriienko <Kupriienko>
2022-07-06 20:52:08 +0300

    updated

--- calculation.js
+++ calculation.js
@@ -1,33 +1,33 @@
-function get_input() {
+function getInput() {
     return document.getElementById("num_expression");
 }
 
 function number(clicked_id) {
-    if (get_input().value === "0" && clicked_id !== "0") {
-        get_input().value = clicked_id;
-    } else if (!((get_input().value === "0" || get_input().value === "") && clicked_id === "0")) {
-        get_input().value += clicked_id;
+    if (getInput().value === "0" && clicked_id !== "0") {
+        getInput().value = clicked_id;
+    } else if (!((getInput().value === "0" || getInput().value === "") && clicked_id === "0")) {
+        getInput().value += clicked_id;
     }
 }
 
 function point() {
-    if (!(get_input().value.includes(".") || get_input().value === "")) {
-        get_input().value += ".";
+    if (!(getInput().value.includes(".") || getInput().value === "")) {
+        getInput().value += ".";
     }
 }
 
 function reset() {
-    get_input().value = "0";
+    getInput().value = "0";
 }
 
 function isNumberKey(evt) {
-    var charCode = (evt.which) ? evt.which : evt.keyCode
-    if (charCode === 48 && get_input().value === "0") {
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode === 48 && getInput().value === "0") {
         return false;
-    } else if (get_input().value === "0" && !(charCode > 31 && (charCode < 48 || charCode > 57))) {
-        get_input().value = String.fromCharCode(charCode)
-        return false
-    } else if (charCode === 46 && !(get_input().value.includes(".") || get_input().value === "")) {
+    } else if (getInput().value === "0" && !(charCode > 31 && (charCode < 48 || charCode > 57))) {
+        getInput().value = String.fromCharCode(charCode);
+        return false;
+    } else if (charCode === 46 && !(getInput().value.includes(".") || getInput().value === "")) {
         return true;
     } else {
         return !(charCode > 31 && (charCode < 48 || charCode > 57));
